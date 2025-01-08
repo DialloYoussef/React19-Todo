6461728a485f3e8f54e98b28cc4cc454594d12fc
Diallo Youssef <dialloyoussouf@gmail.com>
2025-01-08 15:56:21 +0000

    Apprehension des notions de composants avec React et les props, les useStates....

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 function App() {
 
@@ -6,8 +7,14 @@ function App() {
       <h1 className="text-3xl font-bold underline p-2 text-emerald-200">
         Hello world!
       </h1>
+      <button className="bg-red-100 min-h-10 rounded-lg" onClick={() => 
+        console.log("Hello world!")
+      }>
+        Click me!
+      </button>
+      <Button />
       <Lego color="red" />
-      <Lego />
+      <Lego />      
     </div>
   )
 }
@@ -19,8 +26,27 @@ const COLORS = {
   "sky": 'bg-sky-500'
 }
 
+// const [getState, setState] = useState(0);
+function Button() {
+  const [getState, setState] = useState(0);
+  return (
+    <div className="w-full flex items-center justify-center gap-5">
+      <button className="text-center p-2 bg-red-100" onClick={() => setState(getState + 1)}>
+        Incrementer !
+      </button>
+      <span className="rounded-lg p-2 bg-yellow-200">{getState}</span>
+      <button className="text-center p-2 bg-red-100" onClick={() => setState(getState - 1)}>
+        Decrementer
+      </button>
+    </div>
+  )
+}
+
 function Lego( {color = "sky"} ) {
   console.log("color : " + COLORS[color]);
+  const state = useState( 0);
+  console.log("success", state)
+
   return (
     <div className="flex flex-wrap justify-center items-center h-screen">
       <div className={`w-1/3 h-1/3 ${COLORS[color]}`}></div>
