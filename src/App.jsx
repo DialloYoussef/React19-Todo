e3677f28b54efe8769f4f1e6f6aa0b077672081b
Diallo Youssef <dialloyoussouf@gmail.com>
2025-01-09 09:53:45 +0000

    Mise à jour du package.json en ramenant vers 19; et faire le formulaire...

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,78 @@
 import { useState } from "react";
 
-function App() {
+export default function App() {
+ 
+  const [todos, setTodos] = useState();
 
+  const onSubmit = (formData) => {};
   return (
     <div className="bg-gray-800 min-h-[100vh] w-full">
-      <h1 className="text-3xl font-bold underline p-2 text-emerald-200">
+      <h1 className="p-2 text-3xl font-bold underline text-emerald-200">
         Hello world!
       </h1>
-      <button className="bg-red-100 min-h-10 rounded-lg" onClick={() => 
-        console.log("Hello world!")
-      }>
-        Click me!
-      </button>
-      <Button />
-      <Lego color="red" />
-      <Lego />      
+      <div className="m-10">
+        <form
+          action={
+            (formData) => {
+              console.log(formData);
+            }
+          } // Utilisation de l'attribut action avec une fonction
+          className="flex items-center justify-center w-full gap-2 my-5"
+        >
+          <input
+            type="text"
+            name="todo" // Ajout de l'attribut name
+            placeholder="La tâche!!"
+            className="flex-1 p-2 rounded-sm ring-stone-700"
+          />
+          <button type="submit" className="p-2 bg-blue-800 rounded-lg">
+            Ajouter
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-const COLORS = {
-  "red": 'bg-red-500',
-  "blue": 'bg-blue-500',
-  "gray": 'bg-gray-500',
-  "sky": 'bg-sky-500'
-}
 
-// const [getState, setState] = useState(0);
-function Button() {
-  const [getState, setState] = useState(0);
-  return (
-    <div className="w-full flex items-center justify-center gap-5">
-      <button className="text-center p-2 bg-red-100" onClick={() => setState(getState + 1)}>
-        Incrementer !
-      </button>
-      <span className="rounded-lg p-2 bg-yellow-200">{getState}</span>
-      <button className="text-center p-2 bg-red-100" onClick={() => setState(getState - 1)}>
-        Decrementer
-      </button>
-    </div>
-  )
-}
+// 
 
-function Lego( {color = "sky"} ) {
-  console.log("color : " + COLORS[color]);
-  const state = useState( 0);
-  console.log("success", state)
+// LES STATES
+// function Button() {
+//   const [getState, setState] = useState(0);
+//   return (
+//     <div className="flex items-center justify-center w-full gap-5">
+//       <button className="p-2 text-center bg-red-100" onClick={() => setState(getState + 1)}>
+//         Incrementer !
+//       </button>
+//       <span className="p-2 bg-yellow-200 rounded-lg">{getState}</span>
+//       <button className="p-2 text-center bg-red-100" onClick={() => setState(getState - 1)}>
+//         Decrementer
+//       </button>
+//     </div>
+//   )
+// }
 
-  return (
-    <div className="flex flex-wrap justify-center items-center h-screen">
-      <div className={`w-1/3 h-1/3 ${COLORS[color]}`}></div>
-      <div className="w-1/3 h-1/3 bg-green-500"></div>
-      <div className="w-1/3 h-1/3 bg-yellow-50"></div>
-    </div>
-  )
-}
 
-export default App
+// Props 
+// const COLORS = {
+//   "red": 'bg-red-500',
+//   "blue": 'bg-blue-500',
+//   "gray": 'bg-gray-500',
+//   "sky": 'bg-sky-500'
+// }
+// function Lego( {color = "sky"} ) {
+//   console.log("color : " + COLORS[color]);
+//   const state = useState( 0);
+//   console.log("success", state)
+
+//   return (
+//     <div className="flex flex-wrap items-center justify-center h-screen">
+//       <div className={`w-1/3 h-1/3 ${COLORS[color]}`}></div>
+//       <div className="w-1/3 bg-green-500 h-1/3"></div>
+//       <div className="w-1/3 h-1/3 bg-yellow-50"></div>
+//     </div>
+//   )
+// }
+
+
